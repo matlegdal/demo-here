3997b7a98b10a90564f7dd1444065e3f73dd3736
Mathieu Leger-Dalcourt <Mathieu.Leger-Dalcourt@fujitsu.com>
2018-06-04 09:32:27 -0400

    add multiple routes handling

--- app-react/src/Map.jsx
+++ app-react/src/Map.jsx
@@ -12,8 +12,8 @@ export default class Map extends Component {
         });
         this.defaultLayers = this.platform.createDefaultLayers();
         this.state = {
-            markers: [],
-            currentPos: null
+            currentPos: null,
+            currentPosMarker: null
         };
 
         // Bindings des méthodes
@@ -21,8 +21,14 @@ export default class Map extends Component {
     }
 
     componentDidMount() {
-        const fujitsu = { lat: 46.830545, lng: -71.306222 };
-        const map = new window.H.Map(document.getElementById('map'), this.defaultLayers.normal.map, { zoom: 15, center: fujitsu });
+        let pos = { lat: 46.830545, lng: -71.306222 };
+        try {
+            pos = navigator.geolocation.getCurrentPosition();
+        } catch (err) {
+            pos = { lat: 46.830545, lng: -71.306222 };
+        }
+
+        const map = new window.H.Map(document.getElementById('map'), this.defaultLayers.normal.map, { zoom: 15, center: pos });
         const mapEvents = new window.H.mapevents.MapEvents(map);
         const behavior = new window.H.mapevents.Behavior(mapEvents);
         const ui = window.H.ui.UI.createDefault(map, this.defaultLayers, 'fr-FR');
@@ -32,33 +38,39 @@ export default class Map extends Component {
         // Permet le resizing
         window.addEventListener('resize', () => map.getViewPort().resize());
 
-        // Add a marker to fujitsu
-        this.addMarker(map, fujitsu);
-
-        // add marker to current position
+        // add marker to current position and watch the position
         navigator.geolocation.watchPosition(this.updatePosition);
     }
 
     addMarker(map, pos) {
         const marker = new window.H.map.Marker(pos);
         map.addObject(marker);
-        let markers = this.state.markers.slice();
-        markers.push(marker);
-        this.setState({ markers });
+        return marker;
     }
 
     updatePosition(evt) {
         const pos = { lat: evt.coords.latitude, lng: evt.coords.longitude };
         this.setState({ currentPos: pos });
-        this.addMarker(this.state.map, pos);
+        // add marker
+        if (this.state.currentPosMarker) {
+            this.state.map.removeObject(this.state.currentPosMarker);
+        }
+        this.setState({ currentPosMarker: this.addMarker(this.state.map, pos) });
         this.state.map.setCenter(pos);
-        
-        new Route(this.state.map, this.platform, {
-            mode: 'fastest;car',
-            representation: 'display',
-            'waypoint0': coordsToWaypointString(pos),
-            'waypoint1': coordsToWaypointString({ lat: 45.830545, lng: -71.306222 })
-        });
+        // add route
+        const home = { lat: 46.758685, lng: -71.293526 };
+        if (!this.state.routes) {
+            this.drawRoute(home);
+        }
+    }
+
+    drawRoute(toCoords, fromCoords = this.state.currentPos, routeOptions = { mode: 'fastest;car', representation: 'display', alternatives: 2 }) {
+        // add marker to destination 
+        this.addMarker(this.state.map, toCoords);
+        // draw route
+        routeOptions['waypoint0'] = coordsToWaypointString(fromCoords);
+        routeOptions['waypoint1'] = coordsToWaypointString(toCoords);
+        this.setState({ routes: new Route(this.state.map, this.platform, routeOptions) });
     }
 
     render() {
--- app-react/src/Route.js
+++ app-react/src/Route.js
@@ -1,31 +1,10 @@
 export function Route(map, platform, routeOptions) {
     const router = platform.getRoutingService();
     const onSuccess = function (result) {
-        var route,
-            routeShape,
-            strip;
-
         if (result.response.route) {
-            route = result.response.route[0];
-            routeShape = route.shape;
-
-            strip = new window.H.geo.Strip();
-
-            routeShape.forEach(function (point) {
-                var parts = point.split(',');
-                strip.pushLatLngAlt(parts[0], parts[1]);
-            });
-
-            var routeLine = new window.H.map.Polyline(strip, {
-                style: {
-                    strokeColor: 'blue',
-                    lineWidth: 10
-                }
-            });
-
-            map.addObject(routeLine);
-
-            map.setViewBounds(routeLine.getBounds());
+            let routes = result.response.route;
+            routes.forEach((route) => drawRoute(map, route));
+            // map.setViewBounds(routeLine.getBounds());
         }
     };
     const onError = function (err) {
@@ -37,4 +16,23 @@ export function Route(map, platform, routeOptions) {
 
 export function coordsToWaypointString(coords) {
     return `geo!${coords.lat},${coords.lng}`;
+}
+
+function drawRoute(map, route) {
+    let routeShape = route.shape;
+    let strip = new window.H.geo.Strip();
+
+    routeShape.forEach(function (point) {
+        var parts = point.split(',');
+        strip.pushLatLngAlt(parts[0], parts[1]);
+    });
+
+    let routeLine = new window.H.map.Polyline(strip, {
+        style: {
+            strokeColor: 'blue',
+            lineWidth: 3
+        }
+    });
+
+    map.addObject(routeLine);
 }
